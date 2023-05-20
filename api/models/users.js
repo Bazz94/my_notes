51aa9bce700fa2094479fafb62b02695b2ca9e42
Bernard Olivier <88403974+Bazz94@users.noreply.github.com>
2023-05-20 18:37:41 +0200

    Implemented auth route

--- api/models/users.js
+++ api/models/users.js
@@ -1,26 +1,21 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  email: {
+const tagsSchema = new mongoose.Schema({
+  name: {
     type: String,
-    unique: true,
-    required: true, 
+    required: true,
   },
-  password: {
-    type: String,
-    required: true
-  },
-  notes: [notesSchema],
-  tags: [tagsSchema]
 });
 
 const notesSchema = new mongoose.Schema({
-  title: { 
-    type: String, 
-    required: true },
-  content: { 
-    type: Number, 
-    required: true },
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: Number,
+    required: true
+  },
   created: {
     type: Date,
     default: Date.now,
@@ -34,34 +29,18 @@ const notesSchema = new mongoose.Schema({
   tags: [tagsSchema],
 });
 
-const tagsSchema = new mongoose.Schema({
-  name: { 
-    type: String, 
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
     required: true, 
   },
-});
-
-// make email, password and create data read only
-userSchema.pre('save', function (next) {
-  // Check if the immutableField has been modified
-  if (this.isModified('email')) {
-    return next(new Error('email cannot be modified.'));
-  }
-  next();
-  
-  // Check if the immutableField has been modified
-  if (this.isModified('password')) {
-    return next(new Error('password cannot be modified.'));
-  }
-  next();
-});
-
-notesSchema.pre('save', function (next) {
-  // Check if the immutableField has been modified
-  if (this.isModified('created')) {
-    return next(new Error('created date cannot be modified.'));
-  }
-  next();
+  password: {
+    type: String,
+    required: true
+  },
+  notes: [notesSchema],
+  tags: [tagsSchema]
 });
 
 module.exports = mongoose.model('user', userSchema);
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,20 +1,80 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/users.js');
+
+// All users (remove after testing)
+router.get('/all', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 // Login user
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // get email and password from body
-  // check that password is correct
-  // return user id
+  const email = req.body.email;
+  const password = req.body.password;
+  if (email == null) {
+    res.status(400).json({ message: "email is required" });
+  }
+  if (password == null) {
+    res.status(400).json({ message: "password is required" });
+  }
+  // check that credentials are correct
+  try {
+    const users = await User.find({ email: email, password: password});
+    if (users.length == 0) {
+      res.status(400).json({ message: "credentials are incorrect" });
+    }
+    // return user id
+    res.status(200).json({ user_id: users.at(0)._id });
+  } catch (err) {
+    res.status(500).json({message: err.message});
+  }
+  
 });
 
 // Create user
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // get email and password from body
+  const email = req.body.email;
+  const password = req.body.password;
   // check that email is unique 
-  // create user
-  // return user id
+  try {
+    // create user
+    const user = new User({
+      email: email,
+      password: password,
+      notes: [],
+      tags: [] 
+    });
+    const newUser = await user.save();
+    // return user id
+    res.status(201).json(newUser._id);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 });
 
+// Delete user
+router.delete('/', async (req, res) => {
+  // get email and password from body
+  const email = req.body.email;
+  // check that email is unique 
+  try {
+    const users = await User.find({ email: email });
+    if (users.length == 0) {
+      res.status(400).json({ message: "email not found" });
+    }
+    users.at(0).deleteOne();
+    // return user id
+    res.status(200).json({ message: "user deleted"});
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 module.exports = router;
